afda9f5657ceb36d488a89038f60d99f19a93f63
karabenemsi <florian@florianlubitz.de>
2020-10-13 23:15:19 +0200

    Init App

--- src/main/java/logistics/surgerylogistics/App.java
+++ src/main/java/logistics/surgerylogistics/App.java
@@ -1,5 +1,7 @@
 package logistics.surgerylogistics;
 
+import java.util.HashMap;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,27 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Lager lager = new Lager();
+        
+        lager.AnlegenOperation("Crazy Operation", "Arzt", "OTA", "Patient");
+        
+        lager.anzeigeBestandsliste();
     }
+    
+    
+    public void verbrauchtenProdukteAngeben(String [] produkte, int [] anzahlen)
+    {
+        HashMap <String , Integer> liste = new HashMap <String , Integer>();
+
+        for(int i=0; i<produkte.length; i++)
+        {
+            liste.put(produkte[i], anzahlen[i]);   
+        }
+
+        try{
+            lager.ChecklisteBearbeiten(liste);
+        }catch(Exception e)
+        {}
+    }
+
 }
