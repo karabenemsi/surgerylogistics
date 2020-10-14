bcacd209dc1b301b17048179f9a603cc890c78cb
karabenemsi <florian@florianlubitz.de>
2020-10-14 10:04:16 +0200

    Make Storage a singleton

--- src/main/java/logistics/surgerylogistics/App.java
+++ src/main/java/logistics/surgerylogistics/App.java
@@ -7,11 +7,8 @@ package logistics.surgerylogistics;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		Storage lager = new Storage();
-
-		lager.AnlegenOperation(SurgeryType.Herzklappe, "Arzt", "OTA", "Patient");
-
-		System.out.println(lager.anzeigeBestandsliste());
+		Storage storage = Storage.getInstance();
+		System.out.println(storage.anzeigeBestandsliste());
 	}
 
 }
